5b3a958c66bd9f432683904f0835bcd8f929b28f
disintegrating8 <disintegrating8@pm.me>
2025-07-15 12:22:11 +0900

    Update

---
+++ initial-boot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+kvantum_theme="catppuccin-mocha-blue"
+color_scheme="prefer-dark"
+gtk_theme="Flat-Remix-GTK-Blue-Dark"
+icon_theme="Flat-Remix-Blue-Dark"
+cursor_theme="Bibata-Modern-Ice"
+    
+# initiate GTK dark mode and apply icon and cursor theme
+gsettings set org.gnome.desktop.interface color-scheme $color_scheme > /dev/null 2>&1 &
+gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme > /dev/null 2>&1 &
+gsettings set org.gnome.desktop.interface icon-theme $icon_theme > /dev/null 2>&1 &
+gsettings set org.gnome.desktop.interface cursor-theme $cursor_theme > /dev/null 2>&1 &
+gsettings set org.gnome.desktop.interface cursor-size 24 > /dev/null 2>&1 &
+
+# initiate kvantum theme
+kvantummanager --set "$kvantum_theme" > /dev/null 2>&1 &

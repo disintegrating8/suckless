ca8ac3b9bb5f20dc972789ccc64f1c3abdd22329
disintegrating8 <disintegrating8@pm.me>
2025-08-12 16:05:22 +0900

    Update

--- config.h
+++ config.h
@@ -43,6 +43,7 @@ static const char *const autostart[] = {
     "picom", "-b", NULL,
     "sh", "-c", "feh --randomize --bg-fill ~/Pictures/backgrounds/*", NULL,
     "slstatus", NULL,
+    "sh", "-c", "$HOME/suckless/inital-boot.sh", NULL,
     NULL /* terminate */
 };
 
